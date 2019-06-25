b1f990e0829bedec22146df494f25fe1a627ff31
winniethejedi <mcraigparker123@gmail.com>
2019-06-24 22:21:55 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { Observable } from '../../node_modules/rxjs';
+import * as _ from 'lodash';
+
+interface User {
+  name: string,
+  username: string,
+  email: string,
+  address: string,
+  phone: string,
+  website: string,
+  companyName: string,
+  companyBusinessStrategy: string
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +22,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DSTest';
+  private apiUrl = 'https://jsonplaceholder.typicode.com/users';
+  users$: Observable<User[]>;
+
+  constructor(private http:HttpClient) {
+    
+  }
+
+  ngOnInit() {
+    this.users$ = this.http
+      .get<User[]>(this.apiUrl);
+      // .map(data => _.values(data))
+      // .do(console.log);
+  }
 }
